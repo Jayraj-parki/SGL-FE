78b496717f1f469011f04f28213097ccd470d743
Jagadeesh <97141186+Jagadeesh-Udugundla@users.noreply.github.com>
2024-01-25 19:18:44 +0530

    error page check

--- client/src/components/Astrology-demo/zodiacpage/zodiachome.jsx
+++ client/src/components/Astrology-demo/zodiacpage/zodiachome.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../../Perals/PearlsHome.CSS";
+import "../../Perals/PearlsHome.css";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
 
